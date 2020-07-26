e3454ae2ac145930d249675cd0d7df5649029e96
Jared Updike <jupdike@thebrain.com>
2020-07-25 17:34:25 -0700

    Update images

--- make-epub.sh
+++ make-epub.sh
@@ -1,1 +1,1 @@
-(rm -f src/epub/.DS_Store && cd src && zip -r ../hplcf.epub *)
+(rm -f src/epub/images/.DS_Store && rm -f src/.DS_Store && rm -f src/epub/fonts/.DS_Store && rm -f src/epub/.DS_Store && cd src && zip -r ../hplcf.epub *)
